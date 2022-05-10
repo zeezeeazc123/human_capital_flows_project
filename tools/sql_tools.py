c736fcd541a1a2285adc5ae0a961ebbdef1ae9a6
zeezeeazc123 <zeezeecanning@gmail.com>
2022-05-10 11:30:56 -0400

    modified sql_tools

--- tools/sql_tools.py
+++ tools/sql_tools.py
@@ -2,16 +2,31 @@ import getpass
 from sqlalchemy import create_engine
 
 
-def conn_psql(db_user:str, host:str='localhost'):
+def conn_psql(db_user:str, host:str='localhost')->str:
     """Connects to posgreSQL server using SQLAlchemy
     
     Args:
     - db_user (str):    The username for the database connection.       
     - host (str):       Host location
-    Return:
+    
+    Returns:
     - SQLAlchemy Connectable
     """
     
     pwd = getpass.getpass('database user password: ')
     
     return create_engine(f'postgresql+psycopg2://{db_user}:{pwd}@{host}/pitchbook_data').connect()
+
+
+def load_query_from_file(file_path:str):
+    """Loads a query from SQL file as a String for processing with Pandas
+    
+    Args:
+    - file_path (str):  The path to the SQL file
+    
+    Returns:
+    - Query (str)
+    
+    """
+    with open(file_path, 'r') as f:
+        return f.read().replace('\t', '    ')
